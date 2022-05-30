da1fd3d7b1bc54de2495c32311a3a00a6d790480
Noobieprogrammer69 <joververgara1@gmail.com>
2022-05-30 21:11:34 +0800

    chages

--- client/server/server.js
+++ client/server/server.js
@@ -13,12 +13,18 @@ app.use(express.json())
 // app.use(cors());
 app.use(cookieparser());
 
+app.use(express.static("public"));
 app.use('/api', authRouter);
 app.use('/api', userRouter);
+
 
 app.get('/', (req, res) => {
     res.send('App is running')
 })
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/client/public/index.html'));
+});
 
 const port = process.env.PORT || 5000;
 // const URL = process.env.MONGO_URI;
@@ -31,8 +37,8 @@ mongoose.connect("mongodb+srv://login:loginproject@cluster0.qkj5e.mongodb.net/my
     console.log('MongoDb is connected')
 });
 
-const buildPath = path.join(__dirname, '..', 'build');
-app.use(express.static(buildPath));
+// const buildPath = path.join(__dirname, '..', 'build');
+// app.use(express.static(buildPath));
 
 
 // const buildPath = path.join(__dirname, '/client/build');
