6f344a7a347b539e7b0744ed4b451f8e3e6d022b
Noobieprogrammer69 <joververgara1@gmail.com>
2022-05-31 06:50:49 +0800

    change

--- client/server/server.js
+++ client/server/server.js
@@ -13,7 +13,7 @@ app.use(express.json())
 // app.use(cors());
 app.use(cookieparser());
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.use('/api', authRouter);
 app.use('/api', userRouter);
 
@@ -22,9 +22,9 @@ app.get('/', (req, res) => {
     res.send('App is running')
 })
 
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/client/public/index.html'));
-});
+// app.get('*', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/client/public/index.html'));
+// });
 
 const port = process.env.PORT || 5000;
 // const URL = process.env.MONGO_URI;
@@ -44,11 +44,11 @@ mongoose.connect("mongodb+srv://login:loginproject@cluster0.qkj5e.mongodb.net/my
 // const buildPath = path.join(__dirname, '/client/build');
 // app.use(express.static(buildPath));
 
-// app.get(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
 
 // app.get("/", (req, res) => {
 //     res.status(500).send("Hello World");
