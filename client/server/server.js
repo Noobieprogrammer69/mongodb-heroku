2588e5dc33f358f257f7037055089295c748d899
Noobieprogrammer69 <joververgara1@gmail.com>
2022-05-31 07:43:23 +0800

    chnagews please work

--- client/server/server.js
+++ client/server/server.js
@@ -9,6 +9,12 @@ const userRouter = require('./routers/userRouter');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
+
 app.use(express.json())
 // app.use(cors());
 app.use(cookieparser());
@@ -23,12 +29,6 @@ app.use('/api', userRouter);
 
 const port = process.env.PORT || 5000;
 // const URL = process.env.MONGO_URI;
-
-app.use(express.static(path.join(__dirname, "/client/build")));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-});
 
 mongoose.connect("mongodb+srv://login:loginproject@cluster0.qkj5e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
     useNewUrlParser: true,
