3494b087e4efbf16d70bd5ee99c86db9691d8893
Noobieprogrammer69 <joververgara1@gmail.com>
2022-05-30 20:23:01 +0800

    chganges

--- client/server/server.js
+++ client/server/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require("express");
 const mongoose = require('mongoose');
 // const cors = require('cors');
@@ -20,7 +21,7 @@ app.get('/', (req, res) => {
 })
 
 const port = process.env.PORT || 5000;
-const URL = process.env.MONGO_URI;
+// const URL = process.env.MONGO_URI;
 
 mongoose.connect("mongodb+srv://login:loginproject@cluster0.qkj5e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
     useNewUrlParser: true,
