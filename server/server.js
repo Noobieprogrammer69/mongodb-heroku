7dba786ed1462b24a1d8bdd43bfd6078d638edfa
Noobieprogrammer69 <joververgara1@gmail.com>
2022-05-30 19:15:27 +0800

    Changes for heroku

---
+++ server/server.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const express = require("express");
+const mongoose = require('mongoose');
+// const cors = require('cors');
+const cookieparser = require('cookie-parser');
+const authRouter = require('./routers/authRouter');
+const userRouter = require('./routers/userRouter');
+
+const app = express();
+
+app.use(express.json())
+app.use(cors());
+app.use(cookieparser());
+
+app.use('/api', authRouter);
+app.use('/api', userRouter);
+
+app.get('/', (req, res) => {
+    res.send('App is running')
+})
+
+const port = process.env.PORT || 5000;
+const URL = process.env.MONGO_URI;
+
+mongoose.connect("mongodb+srv://login:loginproject@cluster0.qkj5e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, (err) => {
+    if(err) throw err;
+    console.log('MongoDb is connected')
+});
+
+const buildPath = path.join(__dirname, '..', 'build');
+app.use(express.static(buildPath));
+
+// app.get(express.static(path.join(__dirname, "/client/build")));
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+// });
+
+// app.get("/", (req, res) => {
+//     res.status(500).send("Hello World");
+// });
+
+// MONGO_URI = mongodb+srv://login:loginproject@cluster0.qkj5e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+app.listen(port, () => {
+    console.log(`app is running on ${port}`);
+});
