fa3937e4573a9a0bf292592b6191ecca2dcc2c57
Noobieprogrammer69 <joververgara1@gmail.com>
2022-05-31 09:27:16 +0800

    hbgfhfh

--- client/server/routers/userRouter.js
+++ client/server/routers/userRouter.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const auth = require('../middlewares/auth')
 const userCtrl = require('../controllers/userCtrl');
 
-router.get('/user/:id',auth, userCtrl.getUser)
-router.patch('/user',auth, userCtrl.updateUser)
+router.get('/user/:id', auth, userCtrl.getUser)
+router.patch('/user', auth, userCtrl.updateUser)
 
 module.exports = router
--- client/src/utils/fetchDataApi.js
+++ client/src/utils/fetchDataApi.js
@@ -1,7 +1,7 @@
-import axios from './axios';
+import axios from 'axios';
 
 export const getDataApi = async (url, token) =>{
-    const res = await axios.get(`https://mongodb-newsapi.herokuapp.com/api/${url}`,{
+    const res = await axios.get(`/api/${url}`,{
         headers : {Authorization : token}
     })
     return res;
@@ -9,7 +9,7 @@ export const getDataApi = async (url, token) =>{
 
 export const postDataApi =async (url, post, token) => {
    
-    const res = await axios.post(`https://mongodb-newsapi.herokuapp.com/api/${url}`, post ,{
+    const res = await axios.post(`/api/${url}`, post ,{
         headers: {Authorization: token}
 
     })
@@ -19,19 +19,19 @@ export const postDataApi =async (url, post, token) => {
 }
 
 export const putDataApi = async (url , post, token) => {
-    const res = await axios.put(`https://mongodb-newsapi.herokuapp.com/api/${url}`,post,{
+    const res = await axios.put(`/api/${url}`,post,{
         headers :{Authorization: token}
     })
     return res;
 }
 export const patchDataApi = async (url , post, token) => {
-    const res = await axios.patch(`https://mongodb-newsapi.herokuapp.com/api/${url}`, post , {
+    const res = await axios.patch(`/api/${url}`, post , {
         headers :{Authorization: token}
     })
     return res;
 }
 export const deleteDataApi = async (url , token) => {
-    const res = await axios.delete(`https://mongodb-newsapi.herokuapp.com/api/${url}`,{
+    const res = await axios.delete(`/api/${url}`,{
         headers :{Authorization: token}
     })
     return res;
