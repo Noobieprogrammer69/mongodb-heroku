2f3283b591a17600d9146893c8f3e5ca97c707af
Noobieprogrammer69 <joververgara1@gmail.com>
2022-06-04 11:46:07 +0800

    changes

--- server/server.js
+++ server/server.js
@@ -17,19 +17,12 @@ app.use(cookieparser());
 app.use('/api', authRouter);
 app.use('/api', userRouter);
 
-// app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
 
-if(process.env.NODE.ENV === "production") {
-    app.use(express.static("client/build"));
-    const path = require("path");
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-}
 
 // app.get('*', function(req, res) {
 //     res.sendFile(path.join(__dirname + '/client/public/index.html'));
